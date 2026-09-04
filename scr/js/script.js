06a6c925c266e69e0dbe6a3ad9961b7b0463dd01
matthias5823 <matthias@dieorters.de>
2026-09-04 19:56:47 +0200

    change index.html to test.html; added new index.html  on Page folder. Added function loadNavGenres, showBooks on log; added new class in button.css = button-nav / card.css = control-price ; changed in style and

--- scr/js/script.js
+++ scr/js/script.js
@@ -1,1 +1,16 @@
-console.table(books);
+function loadNavGenre() {
+    const navLink = document.getElementById('genre');
+    for (let indexGenres = 0; indexGenres < bookGenres.length; indexGenres++) {
+        
+        navLink.innerHTML += 
+        `<button class="button-nav" onclick="showBooks('${bookGenres[indexGenres]}')">${bookGenres[indexGenres]}</button>`
+    }
+}
+
+function showBooks(genre) {
+  for (let indexBooks = 0; indexBooks < books.length; indexBooks++) {
+    
+    console.log(books[indexBooks].genre);
+    
+  }
+}
