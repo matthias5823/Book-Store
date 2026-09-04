13ffa10d90406e51839d045b5df04354a67d10c5
matthias5823 <matthias@dieorters.de>
2026-09-04 11:35:32 +0200

    first commit for project book store

---
+++ scr/js/varibles.js
@@ -0,0 +1,537 @@
+let books = [
+    {
+      "name": "Fourth Wing – Flammengeküsst",
+      "author": "Rebecca Yarros",
+      "likes": 1250,
+      "liked": true,
+      "price": 16.00,
+      "publishedYear": 2023,
+      "genre": "Fantasy",
+      "picture": "../assets/imgs/FourthWingFlammengeküsst.webp",
+      "comments": [
+        {
+          "name": "Leser123",
+          "comment": "Ein faszinierendes Fantasy-Abenteuer mit Drachen und einer spannenden Geschichte."
+        },
+        {
+          "name": "Bookworm84",
+          "comment": "Die Charaktere und die romantischen Elemente haben mir besonders gut gefallen."
+        },
+        {
+          "name": "FantasyFanatic",
+          "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+        }
+      ]
+    },
+    {
+      "name": "Harry Potter und der Stein der Weisen",
+      "author": "J. K. Rowling",
+      "likes": 1980,
+      "liked": true,
+      "price": 14.00,
+      "publishedYear": 1997,
+      "genre": "Fantasy",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "Leseratte",
+          "comment": "Ein zeitloser Klassiker und ein wunderbarer Einstieg in die Welt der Magie."
+        },
+        {
+          "name": "MagicFan",
+          "comment": "Die Geschichte ist spannend, humorvoll und voller fantastischer Ideen."
+        }
+      ]
+    },
+    {
+      "name": "Der Hobbit",
+      "author": "J. R. R. Tolkien",
+      "likes": 1750,
+      "liked": false,
+      "price": 12.00,
+      "publishedYear": 1937,
+      "genre": "Fantasy",
+      "picture": "../assets/imgs/",
+      "comments": []
+    },
+    {
+      "name": "Die Schule der magischen Tiere",
+      "author": "Margit Auer",
+      "likes": 1120,
+      "liked": true,
+      "price": 13.00,
+      "publishedYear": 2013,
+      "genre": "Fantasy",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "FantasyFan",
+          "comment": "Eine liebevolle Geschichte mit tollen Charakteren und magischen Tieren."
+        }
+      ]
+    },
+    {
+      "name": "Das Lied der Krähen",
+      "author": "Leigh Bardugo",
+      "likes": 1430,
+      "liked": false,
+      "price": 15.00,
+      "publishedYear": 2015,
+      "genre": "Fantasy",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "BookLover",
+          "comment": "Eine düstere und unglaublich spannende Fantasy-Geschichte."
+        },
+        {
+          "name": "ReadingAddict",
+          "comment": "Die Charaktere sind außergewöhnlich und machen die Geschichte besonders."
+        }
+      ]
+    },
+
+    {
+      "name": "Der Vorleser",
+      "author": "Bernhard Schlink",
+      "likes": 1340,
+      "liked": true,
+      "price": 12.00,
+      "publishedYear": 1995,
+      "genre": "Roman",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "Leser123",
+          "comment": "Ein beeindruckender Roman über Schuld, Liebe und die deutsche Vergangenheit."
+        },
+        {
+          "name": "BuchKenner",
+          "comment": "Die Geschichte regt sehr zum Nachdenken an."
+        }
+      ]
+    },
+    {
+      "name": "Der Alchimist",
+      "author": "Paulo Coelho",
+      "likes": 1680,
+      "liked": true,
+      "price": 12.00,
+      "publishedYear": 1988,
+      "genre": "Roman",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "Bookworm84",
+          "comment": "Eine wunderschöne Geschichte über Träume, Ziele und den eigenen Lebensweg."
+        }
+      ]
+    },
+    {
+      "name": "Momo",
+      "author": "Michael Ende",
+      "likes": 1890,
+      "liked": false,
+      "price": 15.00,
+      "publishedYear": 1973,
+      "genre": "Roman",
+      "picture": "../assets/imgs/",
+      "comments": []
+    },
+    {
+      "name": "Der große Sommer",
+      "author": "Ewald Arenz",
+      "likes": 1210,
+      "liked": true,
+      "price": 14.00,
+      "publishedYear": 2021,
+      "genre": "Roman",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "SommerLeser",
+          "comment": "Eine wunderschöne Geschichte über Freundschaft, Erwachsenwerden und den Sommer."
+        }
+      ]
+    },
+    {
+      "name": "Die Vermessung der Welt",
+      "author": "Daniel Kehlmann",
+      "likes": 1090,
+      "liked": false,
+      "price": 13.00,
+      "publishedYear": 2005,
+      "genre": "Roman",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "LiteraturFan",
+          "comment": "Ein intelligenter und gleichzeitig unterhaltsamer Roman."
+        },
+        {
+          "name": "LeserPeter",
+          "comment": "Die historischen Figuren wurden auf eine sehr interessante Weise dargestellt."
+        }
+      ]
+    },
+
+    {
+      "name": "One Piece",
+      "author": "Eiichiro Oda",
+      "likes": 2450,
+      "liked": true,
+      "price": 7.00,
+      "publishedYear": 1997,
+      "genre": "Manga",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "MangaFan",
+          "comment": "Eine unglaublich spannende Reise mit tollen Charakteren und viel Humor."
+        },
+        {
+          "name": "PirateKing",
+          "comment": "Die Welt von One Piece ist riesig und voller Überraschungen."
+        }
+      ]
+    },
+    {
+      "name": "Naruto",
+      "author": "Masashi Kishimoto",
+      "likes": 2310,
+      "liked": true,
+      "price": 7.00,
+      "publishedYear": 1999,
+      "genre": "Manga",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "NinjaFan",
+          "comment": "Eine spannende Geschichte über Freundschaft, Ehrgeiz und Zusammenhalt."
+        }
+      ]
+    },
+    {
+      "name": "Demon Slayer",
+      "author": "Koyoharu Gotouge",
+      "likes": 2180,
+      "liked": false,
+      "price": 8.00,
+      "publishedYear": 2016,
+      "genre": "Manga",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "MangaLover",
+          "comment": "Die Zeichnungen sind beeindruckend und die Geschichte sehr emotional."
+        }
+      ]
+    },
+    {
+      "name": "Attack on Titan",
+      "author": "Hajime Isayama",
+      "likes": 2050,
+      "liked": true,
+      "price": 8.00,
+      "publishedYear": 2009,
+      "genre": "Manga",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "TitanFan",
+          "comment": "Eine düstere Geschichte mit vielen überraschenden Wendungen."
+        },
+        {
+          "name": "Otaku92",
+          "comment": "Spannend von Anfang bis Ende und mit einer außergewöhnlichen Welt."
+        }
+      ]
+    },
+    {
+      "name": "My Hero Academia",
+      "author": "Kohei Horikoshi",
+      "likes": 1960,
+      "liked": false,
+      "price": 7.00,
+      "publishedYear": 2014,
+      "genre": "Manga",
+      "picture": "../assets/imgs/",
+      "comments": []
+    },
+
+    {
+      "name": "Ein ganzes halbes Jahr",
+      "author": "Jojo Moyes",
+      "likes": 1870,
+      "liked": true,
+      "price": 13.00,
+      "publishedYear": 2012,
+      "genre": "Romantik",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "RomanceFan",
+          "comment": "Eine unglaublich emotionale Liebesgeschichte, die mich sehr berührt hat."
+        },
+        {
+          "name": "Leseratte",
+          "comment": "Die Charaktere wirken sehr authentisch und lebendig."
+        }
+      ]
+    },
+    {
+      "name": "Wie ein einziger Tag",
+      "author": "Nicholas Sparks",
+      "likes": 1540,
+      "liked": true,
+      "price": 11.00,
+      "publishedYear": 1996,
+      "genre": "Romantik",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "LoveReader",
+          "comment": "Eine wunderschöne und zeitlose Liebesgeschichte."
+        }
+      ]
+    },
+    {
+      "name": "Bridgerton – Der Duke und ich",
+      "author": "Julia Quinn",
+      "likes": 1720,
+      "liked": false,
+      "price": 12.00,
+      "publishedYear": 2000,
+      "genre": "Romantik",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "BridgertonFan",
+          "comment": "Romantisch, unterhaltsam und mit einer tollen historischen Atmosphäre."
+        }
+      ]
+    },
+    {
+      "name": "The Love Hypothesis",
+      "author": "Ali Hazelwood",
+      "likes": 1630,
+      "liked": true,
+      "price": 14.00,
+      "publishedYear": 2021,
+      "genre": "Romantik",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "RomanceLover",
+          "comment": "Eine humorvolle und moderne Liebesgeschichte mit sympathischen Charakteren."
+        },
+        {
+          "name": "BookGirl",
+          "comment": "Die Mischung aus Wissenschaft und Romance hat mir besonders gefallen."
+        }
+      ]
+    },
+    {
+      "name": "It Starts with Us",
+      "author": "Colleen Hoover",
+      "likes": 1810,
+      "liked": false,
+      "price": 15.00,
+      "publishedYear": 2022,
+      "genre": "Romantik",
+      "picture": "../assets/imgs/",
+      "comments": []
+    },
+
+    {
+      "name": "Der Marsianer",
+      "author": "Andy Weir",
+      "likes": 1930,
+      "liked": true,
+      "price": 14.00,
+      "publishedYear": 2011,
+      "genre": "Science-Fiction",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "SciFiGuru",
+          "comment": "Spannende Science-Fiction mit sehr viel technischem Wissen und schwarzem Humor."
+        },
+        {
+          "name": "SpaceReader",
+          "comment": "Die Geschichte auf dem Mars ist unglaublich fesselnd."
+        }
+      ]
+    },
+    {
+      "name": "Dune – Der Wüstenplanet",
+      "author": "Frank Herbert",
+      "likes": 2140,
+      "liked": true,
+      "price": 15.00,
+      "publishedYear": 1965,
+      "genre": "Science-Fiction",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "SciFiFan",
+          "comment": "Ein absoluter Klassiker mit einer unglaublich detaillierten Welt."
+        },
+        {
+          "name": "Bookworm84",
+          "comment": "Politik, Religion und Science-Fiction werden hervorragend miteinander verbunden."
+        }
+      ]
+    },
+    {
+      "name": "Ready Player One",
+      "author": "Ernest Cline",
+      "likes": 1760,
+      "liked": false,
+      "price": 13.00,
+      "publishedYear": 2011,
+      "genre": "Science-Fiction",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "Gamer123",
+          "comment": "Ein spannendes Abenteuer voller Computerspiele und Popkultur."
+        }
+      ]
+    },
+    {
+      "name": "1984",
+      "author": "George Orwell",
+      "likes": 2280,
+      "liked": true,
+      "price": 12.00,
+      "publishedYear": 1949,
+      "genre": "Science-Fiction",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "Leser123",
+          "comment": "Ein erschreckend aktueller Klassiker über Überwachung und Kontrolle."
+        },
+        {
+          "name": "DystopieFan",
+          "comment": "Eine intensive Geschichte, die lange nach dem Lesen im Kopf bleibt."
+        }
+      ]
+    },
+    {
+      "name": "Die drei Sonnen",
+      "author": "Cixin Liu",
+      "likes": 1490,
+      "liked": false,
+      "price": 16.00,
+      "publishedYear": 2006,
+      "genre": "Science-Fiction",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "ScienceFan",
+          "comment": "Eine faszinierende Geschichte über außerirdisches Leben und die Zukunft der Menschheit."
+        }
+      ]
+    },
+
+    {
+      "name": "Farm der Tiere",
+      "author": "George Orwell",
+      "likes": 1940,
+      "liked": true,
+      "price": 10.00,
+      "publishedYear": 1945,
+      "genre": "Satire",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "LiteraturFan",
+          "comment": "Eine kurze, aber sehr treffende Satire über Macht und politische Systeme."
+        },
+        {
+          "name": "Leseratte",
+          "comment": "Ein Klassiker, der auch heute noch erstaunlich aktuell ist."
+        }
+      ]
+    },
+    {
+      "name": "Der Hundertjährige, der aus dem Fenster stieg und verschwand",
+      "author": "Jonas Jonasson",
+      "likes": 1560,
+      "liked": true,
+      "price": 13.00,
+      "publishedYear": 2009,
+      "genre": "Satire",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "HumorLeser",
+          "comment": "Eine verrückte und sehr unterhaltsame Geschichte mit viel schwarzem Humor."
+        }
+      ]
+    },
+    {
+      "name": "QualityLand",
+      "author": "Marc-Uwe Kling",
+      "likes": 1830,
+      "liked": false,
+      "price": 13.00,
+      "publishedYear": 2017,
+      "genre": "Satire",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "SatireFan",
+          "comment": "Eine witzige und gleichzeitig gesellschaftskritische Geschichte."
+        },
+        {
+          "name": "FutureReader",
+          "comment": "Die Zukunftsvision ist lustig, aber teilweise auch erschreckend realistisch."
+        }
+      ]
+    },
+    {
+      "name": "Er ist wieder da",
+      "author": "Timur Vermes",
+      "likes": 1420,
+      "liked": false,
+      "price": 12.00,
+      "publishedYear": 2012,
+      "genre": "Satire",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "BuchKenner",
+          "comment": "Eine provokante Satire mit vielen gesellschaftlichen Beobachtungen."
+        }
+      ]
+    },
+    {
+      "name": "Die Känguru-Chroniken",
+      "author": "Marc-Uwe Kling",
+      "likes": 2100,
+      "liked": true,
+      "price": 12.00,
+      "publishedYear": 2009,
+      "genre": "Satire",
+      "picture": "../assets/imgs/",
+      "comments": [
+        {
+          "name": "KänguruFan",
+          "comment": "Sehr witzig, absurd und gleichzeitig voller gesellschaftlicher Kritik."
+        },
+        {
+          "name": "LeserPeter",
+          "comment": "Das Känguru ist einfach eine großartige Figur."
+        },
+        {
+          "name": "ComedyReader",
+          "comment": "Perfekte Mischung aus Humor, Alltag und politischer Satire."
+        }
+      ]
+    }
+  ];
+
+let bookGenres = ["alle Genres","Fantasy","Roman","Manga","Romantik", "Science-Fiction","Satire"]
